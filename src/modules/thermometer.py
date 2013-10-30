dc52386033fb8dca4ba0e912ed8e7432fe86fe8a
Magnificus36 <magnificus@outlook.com>
2013-10-30 14:46:13 +0100

    Added init in thermometer

--- src/modules/thermometer.py
+++ src/modules/thermometer.py
@@ -2,7 +2,10 @@ import actions
 import ECA_parser
 import fm
 
+def init(arg):
+	pass
+
 def update_thermometer(gadget_id, split):
-    return actions.update_thermometer(gadget_id, split)
+	return actions.update_thermometer(gadget_id, split)
 
 ECA_parser.functions.update({ "updateThermometer": (2, fm.fcall2(update_thermometer)) })
